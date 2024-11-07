03e7e93aff7a43b25ab48a9b7ae6f899b6705bd5
Mubarek Seid Juhar <mubareksd@gmail.com>
2024-11-07 16:05:39 +0300

    Remove unused imports in FlutterLibserialportPlugin.kt

--- android/src/main/kotlin/org/sigrok/flutter_libserialport/FlutterLibserialportPlugin.kt
+++ android/src/main/kotlin/org/sigrok/flutter_libserialport/FlutterLibserialportPlugin.kt
@@ -1,13 +1,11 @@
 package org.sigrok.flutter_libserialport
 
 import androidx.annotation.NonNull
-
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
-import io.flutter.plugin.common.PluginRegistry.Registrar
 
 /** FlutterLibserialportPlugin */
 class FlutterLibserialportPlugin: FlutterPlugin, MethodCallHandler {
